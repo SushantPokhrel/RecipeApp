1fde8ff83e82c54d0e8ec70c1bf09dd55d9c4e51
SushantPokhrel <sushant.805421@cct.tu.edu.np>
2024-08-10 13:33:42 +0545

    made responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 
 export default function Nav(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
   function handleContact() {
     props.setRenderMain((pre) => !pre);
   }
-  console.log(props.render);
+
+  function handleNavBar() {
+    setIsOpen((pre) => !pre);
+  }
+
   return (
     <div>
       <nav className="nav-bar">
-        <h1 className="logo">Recipe.np</h1>
-        <ul>
+        <h1 className="logo" onClick={()=>location.reload()}>Recipe.np</h1>
+        <ul className={isOpen ? "sm-ul" : ""}>
+          <span className="cross-icon" onClick={()=>setIsOpen(pre=>!pre)}>X</span>
           <li>
             <a href="index.html">Home</a>
           </li>
           <li>
-            <a href="#" 
-            onClick={handleContact}
-            >
+            <a href="#" onClick={handleContact}>
               Contact Us
             </a>
           </li>
@@ -24,6 +29,11 @@ export default function Nav(props) {
             <a href="#">About Us</a>
           </li>
         </ul>
+        <div className="hamburger" onClick={handleNavBar}>
+          <button className="btn-bars">
+            <i className="fa fa-bars" aria-hidden="true"></i>
+          </button>
+        </div>
       </nav>
     </div>
   );
